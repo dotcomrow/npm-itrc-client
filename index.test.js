85ec8ca41293b1b6918987a010e92311ba77d525
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-03-05 19:28:05 +0000

    initial commit
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

---
+++ index.test.js
@@ -0,0 +1,7 @@
+const add = require('./commands/add');
+const list = require('./commands/list');
+const markDone = require('./commands/markDone');
+
+test('loads', () => {
+    expect(1).toBe(1);
+});
