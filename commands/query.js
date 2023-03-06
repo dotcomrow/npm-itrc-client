4dc4fe42329c556e4887b5124feaacaee6151615
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-03-06 14:09:07 +0000

    performing update

--- commands/query.js
+++ commands/query.js
@@ -1,9 +1,28 @@
-const getConnection = require('../utilities/connection')
+const getTokenConnection = require('../utilities/connection')
 
 function query (args) {
-    getConnection(args).then((token) => {
-        console.log(token)
-    });
+    new Promise((resolve, reject) => {
+        var options = {
+            'method': 'POST',
+            'hostname': 'websec.cable.comcast.com',
+            'path': '/as/token.oauth2?grant_type=client_credentials&client_id=' + args.user + '&client_secret=' + args.password + '&scope=itrc:user',
+            'maxRedirects': 20
+        }
+
+        const handleRequest = (options, {}) => {
+            const reqt = https.request(options);
+            console.log(body)
+        };
+
+        const req2 = https.request({});
+        req2.once("response", handleRequest);
+
+        if (body) {
+            req2.end(body);
+        } else {
+            req2.end();
+        }
+  });
 }
 
 module.exports = query
--- utilities/connection.js
+++ utilities/connection.js
@@ -3,15 +3,8 @@ const {https} = require( "https");
 
 function req  (urlOptions, body) {
   new Promise((resolve, reject) => {
-        var options = {
-            'method': 'POST',
-            'hostname': 'websec.cable.comcast.com',
-            'path': '/as/token.oauth2?grant_type=client_credentials&client_id=' + args.user + '&client_secret=' + args.password + '&scope=itrc:user',
-            'maxRedirects': 20
-        }
-
-        const handleRequest = (options, {}) => {
-            const reqt = https.request(options);
+        const handleRequest = (urlOptions, {}) => {
+            const reqt = https.request(urlOptions);
             resolve(body.access_token)
         };
 
