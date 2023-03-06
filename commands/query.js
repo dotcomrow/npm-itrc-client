8aa851dcf7e88d7dd26d871c7953354e6f3a3e7d
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-03-06 14:31:48 +0000

    performing update

--- commands/query.js
+++ commands/query.js
@@ -3,27 +3,60 @@ const https = require('https');
 
 function query (args) {
     new Promise((resolve, reject) => {
-        var options = {
+        const handleRequestResponse = (res) => {
+          removeRequestListeners();
+    
+          const chunks = [];
+          const handleResponseData = (chunk) => {
+            chunks.push(chunk);
+          };
+    
+          const handleResponseError = (err) => {
+            removeResponseListeners();
+            reject(err);
+          };
+    
+          const handleResponseEnd = () => {
+            removeResponseListeners();
+            resolve({ req, res, body: Buffer.concat(chunks) });
+          };
+    
+          const removeResponseListeners = () => {
+            res.removeListener("data", handleResponseData);
+            res.removeListener("error", handleResponseError);
+            res.removeListener("end", handleResponseEnd);
+          };
+    
+          res.on("data", handleResponseData);
+          res.once("error", handleResponseError);
+          res.once("end", handleResponseEnd);
+        };
+    
+        const handleRequestError = (err) => {
+          removeRequestListeners();
+          reject(err);
+        };
+    
+        const removeRequestListeners = () => {
+          req.removeListener("response", handleRequestResponse);
+          req.removeListener("error", handleRequestError);
+        };
+        urlOptions= {
             'method': 'POST',
             'hostname': 'websec.cable.comcast.com',
             'path': '/as/token.oauth2?grant_type=client_credentials&client_id=' + args.user + '&client_secret=' + args.password + '&scope=itrc:user',
             'maxRedirects': 20
         }
-
-        const handleRequest = (options, {}) => {
-            const reqt = https.request(options);
-            console.log(body)
-        };
-
-        const req2 = https.request({});
-        req2.once("response", handleRequest);
-        body={}
+        const req = https.request(urlOptions);
+        req.once("response", handleRequestResponse);
+        req.once("error", handleRequestError);
+    
         if (body) {
-            req2.end(body);
+          req.end(body);
         } else {
-            req2.end();
+          req.end();
         }
-  });
+    })
 }
 
 module.exports = query
