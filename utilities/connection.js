6b19f299c6c325ac3bb8356a6564b50ab446719e
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-03-06 03:35:19 +0000

    performing update

--- utilities/connection.js
+++ utilities/connection.js
@@ -1,19 +1,29 @@
 const req = import('../utilities/ConnectionWrapper')
 
-function getConnection (args, doNext) {
+function getConnection (args) {
+    new Promise((resolve, reject) => {
+        var options = {
+            'method': 'POST',
+            'hostname': 'websec.cable.comcast.com',
+            'path': '/as/token.oauth2?grant_type=client_credentials&client_id=' + args.user + '&client_secret=' + args.password + '&scope=itrc:user',
+            'maxRedirects': 20
+        }
+
+        const handleRequest = (options, {}) => {
+            const req = https.request(urlOptions);
+            resolve{(body.access_token})
+        };
+
+        
+        req.once("response", handleRequestResponse);
+        req.once("error", handleRequestError);
     
-    var options = {
-        'method': 'POST',
-        'hostname': 'websec.cable.comcast.com',
-        'path': '/as/token.oauth2?grant_type=client_credentials&client_id=' + args.user + '&client_secret=' + args.password + '&scope=itrc:user',
-        'maxRedirects': 20
-    }
-
-    req(options, {}).then((reqt, res, body) => {
-        console.log(body)
-        doNext(body.access_token)
-    })
-
+        if (body) {
+          req.end(body);
+        } else {
+          req.end();
+        }
+    });
 }
 
 module.exports = getConnection
