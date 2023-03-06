04c8b3f0cbb32f579554dbae007698cc036b60ae
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-03-06 02:19:59 +0000

    performing update

--- commands/query.js
+++ commands/query.js
@@ -1,6 +1,8 @@
 const getConnection = require('../utilities/connection')
 
-function query (args) {
+function query (args, arg2) {
+    console.log(args)
+    console.log(arg2)
     getConnection(args, (token) => {
         console.log(token)
     });
