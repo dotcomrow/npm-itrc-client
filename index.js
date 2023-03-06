ec93eb8e04fafc3d9e071bfa9d010c09277b4470
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-03-06 18:12:16 +0000

    performing update

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const query = require('./commands/query')
 
 program
     .command('query')
-    .option('-a, --api <ITRC API Endpoint>', 'HTTPS ITRC API Endpoint')
-    .option('-u, --user <ITRC User>', 'ITRC API Enpoint User')
-    .option('-p, --password <ITRC Password>', 'ITRC API Password')
-    .option('-q, --query <ITRC Query String>', 'ITRC API Query string')
+    .option('-a, --api <ITRC API Endpoint>', 'HTTPS ITRC API Endpoint').requiredOption(true)
+    .option('-u, --user <ITRC User>', 'ITRC API Enpoint User').requiredOption(true)
+    .option('-p, --password <ITRC Password>', 'ITRC API Password').requiredOption(true)
+    .option('-q, --query <ITRC Query String>', 'ITRC API Query string').requiredOption(true)
     .description('query ITRC system and return values in query string')
     .action(query)
 
