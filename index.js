956b6c5c1117e155cc7c0695590a5d373e74da7d
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-03-06 18:40:54 +0000

    performing update

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const query = require('./commands/query')
 
 program
     .command('query')
-    .option('-a, --api <ITRC API Endpoint>', 'HTTPS ITRC API Endpoint').requiredOption('<api>')
-    .option('-u, --user <ITRC User>', 'ITRC API Enpoint User').requiredOption('<user>')
-    .option('-p, --password <ITRC Password>', 'ITRC API Password').requiredOption('<password>')
-    .option('-q, --query <ITRC Query String>', 'ITRC API Query string').requiredOption('<query>')
+    .option('-a, --api <ITRC API Endpoint>', 'HTTPS ITRC API Endpoint')
+    .option('-u, --user <ITRC User>', 'ITRC API Enpoint User')
+    .option('-p, --password <ITRC Password>', 'ITRC API Password')
+    .option('-q, --query <ITRC Query String>', 'ITRC API Query string')
     .description('query ITRC system and return values in query string')
-    .action(query)
+    .action((args) => {
+        if (
+            /[\S\s]+[\S]+/.test(args.api) &&
+            /[\S\s]+[\S]+/.test(args.user) &&
+            /[\S\s]+[\S]+/.test(args.password) &&
+            /[\S\s]+[\S]+/.test(args.query)
+        ) {
+            process.exitCode="Not enough arguments were set.  Please re-run with -h to view arguments."
+            process.exit();
+        } else {
+            query
+        }
+    })
 
 program
     .command('version')
