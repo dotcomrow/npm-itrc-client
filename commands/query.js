29f26b0f53f72e46705f88002917e911f06efeca
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-03-06 15:27:34 +0000

    performing update

--- commands/query.js
+++ commands/query.js
@@ -1,60 +1,21 @@
-const getTokenConnection = require('../utilities/connection')
-const https = require('https');
+const conWrapper = require('../utilities/ConnectionWrapper')
 
 function query (args) {
-    new Promise((resolve, reject) => {
-        const handleRequestResponse = (res) => {
-          removeRequestListeners();
-    
-          const chunks = [];
-          const handleResponseData = (chunk) => {
-            chunks.push(chunk);
-          };
-    
-          const handleResponseError = (err) => {
-            removeResponseListeners();
-            reject(err);
-          };
-    
-          const handleResponseEnd = () => {
-            removeResponseListeners();
-            console.log(Buffer.concat(chunks).toString())
-            // got access getTokenConnection, call itrc
-            resolve({ req, res, body: Buffer.concat(chunks) });
-          };
-    
-          const removeResponseListeners = () => {
-            res.removeListener("data", handleResponseData);
-            res.removeListener("error", handleResponseError);
-            res.removeListener("end", handleResponseEnd);
-          };
-    
-          res.on("data", handleResponseData);
-          res.once("error", handleResponseError);
-          res.once("end", handleResponseEnd);
-        };
-    
-        const handleRequestError = (err) => {
-          removeRequestListeners();
-          reject(err);
-        };
-    
-        const removeRequestListeners = () => {
-          req.removeListener("response", handleRequestResponse);
-          req.removeListener("error", handleRequestError);
-        };
-        urlOptions= {
-            'method': 'POST',
-            'hostname': 'websec.cable.comcast.com',
-            'path': '/as/token.oauth2?grant_type=client_credentials&client_id=' + args.user + '&client_secret=' + args.password + '&scope=itrc:user',
-            'maxRedirects': 20
-        }
-        const req = https.request(urlOptions);
-        req.once("response", handleRequestResponse);
-        req.once("error", handleRequestError);
 
-        req.end();
-    })
+
+    urlOptions= {
+        'method': 'POST',
+        'hostname': 'websec.cable.comcast.com',
+        'path': '/as/token.oauth2?grant_type=client_credentials&client_id=' + args.user + '&client_secret=' + args.password + '&scope=itrc:user',
+        'maxRedirects': 20
+    }
+
+    resp = conWrapper(urlOptions, {});
+    
+
+    access_token=resp.access_token;
+    console.log("Access Token to itrc -> " + access_token)
+
 }
 
 module.exports = query
